60e214da8ae672bb9a9603733c6bd69e06ea3124
Lily <171607829@qq.com>
2016-01-29 21:02:34 +0800

    perfecting Login

---
+++ JSOA/JsCommon/JsCommonMain.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class JsCommon_JsCommonMain : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string jobName=Request.Params["Name"];
+        Response.Write("欢迎" + jobName + "!");
+        //Response.Write("555");
+    }
+}
